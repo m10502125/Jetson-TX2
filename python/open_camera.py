b609debd0d59855333dfeb1a7d033fd44094f396
SeanYao <b11235813213445@gmail.com>
2017-06-20 17:02:18 +0800

    Open the camera on jetson TX2
    Open the build-in TX2 camera by the open_camera.py,
    and show the original and grayscale image on the screen.

---
+++ python/open_camera.py
@@ -0,0 +1,22 @@
+import sys
+import cv2
+
+def read_cam():
+
+    cap = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+    if cap.isOpened():
+        cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
+        while True:
+            ret_val, img = cap.read();
+	    cv2.imshow('demo',img)
+	    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	    cv2.imshow('demo2',img_gray)
+            cv2.waitKey(10)
+    else:
+     print "camera open failed"
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    read_cam()
