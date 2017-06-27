25a2a9b42bd5ede654aa605115a130899f947e2a
SeanYao <b11235813213445@gmail.com>
2017-06-27 16:35:23 +0800

    Open the USB camera successfully.
    ls /dev/video*
    
    On-board camera -- /dev/video0
    Other USB cameras -- /dev/video1
    
    Set resolution to (1920,1080)
    cap.set(3,*)
    cap.set(4,*)
    
    Calculate FPS
    FPS = 1/(endTime-startTime)

--- python/open_camera.py
+++ python/open_camera.py
@@ -3,7 +3,7 @@ import cv2
 
 def read_cam():
 
-    cap = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+    cap = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
     if cap.isOpened():
         cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
         while True:
--- python/open_otherCamera.py
+++ python/open_otherCamera.py
@@ -3,18 +3,24 @@
 
 import sys
 import cv2
-
+import time
 def read_cam():
 
-    cap = cv2.VideoCapture(0);
+    cap = cv2.VideoCapture(1);
+    cap.set(3,1920);
+    cap.set(4,1080);
+    
     if cap.isOpened():
         cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
         while True:
+            start = time.time();
             ret_val, img = cap.read();
 	    cv2.imshow('demo',img)
-	    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	    cv2.imshow('demo2',img_gray)
+	    '''img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	    cv2.imshow('demo2',img_gray)'''
             cv2.waitKey(10)
+            end=time.time();
+            print (1/(end-start))
     else:
      print "camera open failed"
 
