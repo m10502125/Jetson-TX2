3df8180bcbd9d2aefcae78c70aa3ee23ec015aa0
SeanYao <b11235813213445@gmail.com>
2017-06-28 13:53:42 +0800

    Gray and sobel_x operation.
    Converter RGB image to grayscale image and do the sobel operation.

--- python/open_otherCamera.py
+++ python/open_otherCamera.py
@@ -4,20 +4,27 @@
 import sys
 import cv2
 import time
+import numpy as np
 def read_cam():
 
     cap = cv2.VideoCapture(1);
-    cap.set(3,1920);
-    cap.set(4,1080);
+    '''cap.set(3,1920);
+    cap.set(4,1080);'''
+    cap.set(3,1280);
+    cap.set(4,720);
     
     if cap.isOpened():
         cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
         while True:
             start = time.time();
-            ret_val, img = cap.read();
+            #cap.read() will return a tuple, flag and image.		
+            ret_val,img = cap.read();
 	    cv2.imshow('demo',img)
-	    '''img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	    cv2.imshow('demo2',img_gray)'''
+	    #Converter RGB image to grayscale image.
+	    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	    #Sobel operation.
+	    sobelx = cv2.Sobel(img_gray,cv2.CV_64F,1,0,ksize=5)
+	    cv2.imshow('sobelx',sobelx)
             cv2.waitKey(10)
             end=time.time();
             print (1/(end-start))
